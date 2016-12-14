28e0396c4598812e7e2b72a4d8a9c361ad8ed6fa
Kim Maida <kim.maida@auth0.com>
2016-12-14 14:57:30 -0500

    Add props param to User component render function.

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -5,13 +5,13 @@ import Component from 'inferno-component';
 import './User.css';
 
 class User extends Component {
-  render() {
-    let profile = this.props.profile;
+  render(props) {
+    let profile = props.profile;
     let idp = profile.user_id.split('|')[0];
 
     return(
       <div className="User" title={idp}>
-        <img src={profile.picture} alt={idp} />
+        <img src={profile.picture} alt={profile.name} />
         <span>{profile.name}</span>
       </div>
     );
