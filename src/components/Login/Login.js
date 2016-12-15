ec4483c3478eebfd46a770a53ac80562e5879728
Kim Maida <kim.maida@auth0.com>
2016-12-15 10:25:33 -0500

    Pulled out the login method (also doesn’t need to be a method on class).

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,17 +4,17 @@ import Inferno, { linkEvent } from 'inferno';
 import Component from 'inferno-component';
 import './Login.css';
 
-class Login extends Component {
-  // Use the "lock" prop passed in App.js to
-  // show the Auth0 Lock widget so users can log in
-  showLock(instance) {
-    instance.props.lock.show();
-  }
+// Use the "lock" prop passed in App.js to
+// show the Auth0 Lock widget so users can log in
+function showLock(instance) {
+  instance.props.lock.show();
+}
 
+class Login extends Component {
   render() {
     return(
       <div className="Login">
-        <a onClick={linkEvent(this, this.showLock)}>Log In</a>
+        <a onClick={linkEvent(this, showLock)}>Log In</a>
       </div>
     );
   }
